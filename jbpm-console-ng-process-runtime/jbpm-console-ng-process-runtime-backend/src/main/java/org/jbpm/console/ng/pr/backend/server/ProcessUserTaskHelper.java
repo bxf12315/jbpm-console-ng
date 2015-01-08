a724f3e216b32261a79569e4d9da50d51f9bde4c
xiabai <xiabai@dhcp-137-101.nay.redhat.com>
2015-01-08 12:46:18 +0800

    BZ-1165089 - [RFE]process initiator（or process admin） can not check process instance history details                  1 add usertask log tab page

---
+++ jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-backend/src/main/java/org/jbpm/console/ng/pr/backend/server/ProcessUserTaskHelper.java
@@ -0,0 +1,31 @@
+package org.jbpm.console.ng.pr.backend.server;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import org.jbpm.console.ng.pr.model.UserTaskSummary;
+import org.kie.api.task.model.TaskSummary;
+
+public class ProcessUserTaskHelper {
+	public static List<UserTaskSummary> adaptCollection(Collection<TaskSummary> taskSummary) {
+        List<UserTaskSummary> userTaskSummary = new ArrayList<UserTaskSummary>();
+        for (TaskSummary ts : taskSummary) {
+        	userTaskSummary.add(adapt(ts));
+        }
+
+        return userTaskSummary;
+    }
+
+    public static UserTaskSummary adapt(TaskSummary ts) {
+    
+        return new UserTaskSummary(ts.getId(), ts.getName(), ts.getActualOwnerId(), ts.getStatusId(), ts.getDescription(),ts.getCreatedById(),
+                  ts.getCreatedOn(),ts.getActivationTime(),ts.getExpirationTime());
+        
+    }
+    
+/*    public UserTaskSummary(Long id, String name, String owner, String status,
+			String description, String createdBy,
+			Date createdOn, Date activationTime, Date expirationTime) {*/
+}
